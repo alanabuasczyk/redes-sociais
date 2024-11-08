4ee7e69b3fa0ab8b43c4e74c9d94a3200d66d446
alanabuasczyk <129075252+alanabuasczyk@users.noreply.github.com>
2024-11-08 09:26:16 -0300

    site

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -6,6 +6,20 @@ const dados = await res.json()
 
 console.log(dados)
 const nomesDasRedes = Object.keys(dados)
-const quantidadeUsuarios = 
+const quantidadeUsuarios = Object.values(dados)
+
+const data = [
+}
+x: nomeDasRedes,
+y: quantidadeUsuarios,
+type: 'bar'
+}
+]
+
+const grafico = document.createElement('div')
+grafico.className = 'grafico'
+document.getElementById('graficos-container').appendChild(grafico)
+Plotly.newPlot(grafico.data)
+
 }
 quantidadeUsuarios()
